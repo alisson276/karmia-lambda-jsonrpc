43c4a55c3d37d6e1ba90d84ee4ca47133c59931f
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2018-04-02 01:47:22 +0000

    Add ts.d file

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+import {KarmiaConverterJSONRPC} from "karmia-converter-jsonrpc";
+
+declare module KarmiaLambdaJSONRPC {
+    class KarmiaLambdaJSONRPCMethod {
+        converter: KarmiaConverterJSONRPC;
+        methods: Object;
+
+        constructor(options: Object|null);
+        set(key: object|string, value: Function|null|undefined): KarmiaLambdaJSONRPCMethod;
+        clear(): KarmiaLambdaJSONRPCMethod;
+        get(path: null|string|undefined): Function|Object|undefined;
+        list(): Object;
+        call(event: Object, context: Object, body: Array<Object>|Object, parameters: Object): Promise;
+    }
+
+    export class KarmiaLambdaJSONRPC {
+        methods: KarmiaLambdaJSONRPCMethod;
+        converter: KarmiaConverterJSONRPC;
+        parameters: Object;
+
+        constructor(methods: Object|null|undefined);
+        set(key: Object|string, value:any): KarmiaLambdaJSONRPC;
+        clear(): KarmiaLambdaJSONRPC;
+        get(path: null|string|undefined): any;
+        list(): Object;
+        call(event: Object, context: Object, body: Array<Object>|Object): Promise;
+    }
+}
